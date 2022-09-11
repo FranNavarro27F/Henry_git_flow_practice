7f2a35de22773a8cd06432b26f422656287210cd
Francisco Navarro <fran27_navarro@hotmail.com>
2022-09-10 23:12:55 -0300

    conflicto, espero

--- index.js
+++ index.js
@@ -28,6 +28,9 @@ function laCajaDePandora(numero){
                 }
             }
              return arrHex
+             //asfasg
+             //fasfayh
+             ///asgh
         }
 
     }
