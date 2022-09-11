753440ffb1df9fab6c3dd5aa3294476faf5063e2
Mary Kuttel <mcandekuttel@gmail.com>
2022-09-10 22:52:07 -0300

    tenga hermoso

--- index.js
+++ index.js
@@ -1,5 +1,36 @@
 function laCajaDePandora(numero){
     // proximamente escribiremos codigo aqui
 
+    let arrBin = ''
+    let arrHex = ''
+    let hexas = [10, 11, 12, 13, 14, 15]
+    let hexasLet = ['A', 'B', 'C', 'D', 'E', 'F']
 
+    if(numero % 2 === 0){
+
+        while(numero > 0){
+            let rest = numero % 2;
+            arrBin = rest + arrBin;
+            numero = Math.floor(numero/2);
+        }
+
+    return arrBin
+
+    }else{
+        while(numero > 0){
+            let rest = numero % 16;
+            if(rest > 9){
+                let index = hexas.indexOf(rest)
+                arrHex = hexasLet[index] + arrHex
+                numero = Math.floor(numero/16)
+            }else{
+                arrHex = rest + arrHex
+                numero = Math.floor(numero/16)
+            }
+        }
+    return arrHex
     }
+    }
+
+console.log(laCajaDePandora(451))
+    
